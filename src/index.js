ff8b4b10a93ec0b1ea330b1bd97e11d712bd571a
LesiaBelinska <lm.kudak@gmail.com>
2022-04-01 01:14:55 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-import './sass/main.scss';
+import dishes from './menu.json';
+import itemsTemplate from './template.hbs';
+import './styles.css';
+
+const menuRef = document.querySelector('.js-menu');
+const bodyRef = document.querySelector('body');
+const checkboxRef = document.querySelector('#theme-switch-toggle');
+
+
+const markup = itemsTemplate(dishes);
+
+menuRef.insertAdjacentHTML('beforeend', markup);
+
+checkboxRef.addEventListener('change', onCheckboxChange);
+
+// function onCheckboxChange(event){
+//     bodyRef.classList.toggle('dark-theme');
+// };
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+function onCheckboxChange(event) {
+    if (event.currentTarget.checked) {
+        bodyRef.classList.remove(Theme.LIGHT);
+        bodyRef.classList.add(Theme.DARK);
+        localStorage.setItem('theme', Theme.DARK);
+    } else {
+        bodyRef.classList.remove(Theme.DARK);
+        bodyRef.classList.add(Theme.LIGHT);
+        localStorage.setItem('theme', Theme.LIGHT);
+    }
+}
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme) {
+    bodyRef.classList.add(savedTheme);
+    if (savedTheme === Theme.DARK) {
+        checkboxRef.checked = true;
+    }
+}
